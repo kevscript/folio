56d20c4b1ff63ff996c96ea4963a75287a2d7fb7
Kevin Ostafinski <kevscripted@gmail.com>
2024-06-06 19:00:13 +0200

    added responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,16 @@ export function Footer() {
   return (
     <footer>
       <Container className="flex flex-col gap-16">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-center gap-4 md:justify-between items-center">
           <Socials />
 
           <a href="/resume.pdf" target="_blank">
             <SecondaryButton icon={<DownloadIcon />}>Resume</SecondaryButton>
           </a>
         </div>
-        <div className="flex justify-between items-center text-zinc-500">
+        <div className="flex flex-col md:flex-row gap-4 md:justify-between items-center text-zinc-500">
           <span>2024 - All rights reserved</span>
-          <span>Made with :brain: & :hands:</span>
+          <span>kevinostafinski@gmail.com</span>
         </div>
       </Container>
     </footer>
